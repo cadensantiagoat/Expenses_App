a2516bd7352220ae0d0e89b49855b137ce9d9d66
Jared Cooke <jaredmatthewcooke@gmail.com>
2024-09-19 15:02:19 -0700

    Installed Clerk and setup basic authentication using their provider and middleware. Also added a temporary header and changed some CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="h-[calc(100vh-74px)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Expense Tracker</h1>
-        <p>This will be the login page.</p>
+        <h1>Home (for signed-in users)</h1>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        this is the footer
-      </footer>
     </div>
   );
 }
