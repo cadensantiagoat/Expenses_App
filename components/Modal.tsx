ffa138af52923e34f7c3477ade089f37347d2b4a
Jared Cooke <46581397+JaredMCooke@users.noreply.github.com>
2024-12-04 15:03:17 -0800

    Made modal container scrollable and adjusted the height.

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -29,13 +29,13 @@ const ModalContent = ({
 }) => {
   return (
     <DialogPrimitive.Portal>
-      <DialogPrimitive.Overlay className='fixed inset-0 z-10 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0' />
+      <DialogPrimitive.Overlay className='fixed inset-0 z-10 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0' />
 
       <DialogPrimitive.Content
         aria-describedby={undefined}
         onInteractOutside={onInteractOutside}
         className={
-          'fixed left-[50%] top-[50%] z-20 grid w-full max-w-xl translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-3 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg'
+          'fixed left-[50%] top-[50%] z-20 grid w-full h-[90%] max-w-xl translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-3 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg'
         }
       >
         <div className='modal-content-wrapper overflow-auto max-h-[900px] p-5'>
